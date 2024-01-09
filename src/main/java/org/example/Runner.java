1e66f143959ea1a3bea47941cdad2723e5de3f0b
gokcelal <arslan.lalgokce@gmail.com>
2024-01-09 17:09:16 +0300

    Initial commit.

--- src/main/java/org/example/Runner.java
+++ src/main/java/org/example/Runner.java
@@ -1,30 +1,53 @@
 package org.example;
 
 import org.example.entity.Contact;
+import org.example.entity.Lesson;
 import org.example.entity.Student;
+import org.example.entity.Teacher;
+import org.example.repository.LessonRepository;
 import org.example.repository.StudentRepository;
 
 public class Runner {
     public static void main(String[] args) {
 
+        Lesson lesson = new Lesson();
+        lesson.setName("Matematik");
+        //lesson.setTeacher(teacher);
 
-    }
-    private static void ogrenciEkle(){
+        Contact contact = new Contact();
+        contact.setCity("Eskisehir");
+        contact.setZipcode("1234");
+        contact.setPhone("055506");
+        contact.setParent("Gokce");
 
-        Student student = Student.builder()
-                .name("Deniz")
-                .surname("Bektas")
-                .build();
-        Contact contact = Contact.builder()
-                .city("Eskisehir")
-                .zipcode("0203")
-                .phone("0 555 666 77 88")
-                .parent("Gokce")
-                .build();
+        Student student = new Student();
 
+        student.setName("Deniz");
+        student.setSurname("Bektas");
+        student.setContact(contact);
+       // student.setLessonList();
 
 
         contact.setStudent(student);
+
+
+        Teacher teacher = new Teacher();
+        teacher.setName("Aslı");
+
+
+
+
+
+        teacher.setLesson(lesson);
         new StudentRepository().save(student);
+        new LessonRepository().save(lesson);
+
     }
+
+
+
+
+
+
+
 }
